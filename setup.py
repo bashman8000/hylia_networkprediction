4254a699b121dd3f3ae57e248f27ecc59f4b2ead
Bashir M <bmohammed@lbl.gov>
2021-05-25 10:38:58 -0700

    changed maintaner

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
       long_description_content_type="text/markdown",
       project_urls=PROJECT_URLS,
       url=URL,
-      maintainer='Daphne',
+      maintainer='daphne-lab (mkrian@sn.net)',
       maintainer_email='mkiran@es.net',
       license='Lawrence Berkeley National Laboratory',
       packages=find_packages(),
